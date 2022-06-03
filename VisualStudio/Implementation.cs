eaaa4888f6f157b3541d5dd07ae0e9fe3d93adf3
JTspectre <104400219+JTspectre@users.noreply.github.com>
2022-06-03 03:17:11 -0400

    ...

---
+++ VisualStudio/Implementation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ModSettings;
+using System.Collections.Generic; // List<>
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+namespace JTspectreItemPack
+{
+    internal class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Settings.OnLoad();/// ModSettings
+            LoggerInstance.Msg($"JTspectreItemPack - Version {BuildInfo.Version}");
+        }
+    }
+}
---
+++ VisualStudio/Settings.cs
@@ -0,0 +1,37 @@
+﻿using ModSettings;
+using UnityEngine;
+using MelonLoader;
+
+namespace JTspectreItemPack
+{
+    internal class DiySettings : JsonModSettings
+    {
+        [Section("Colliders")]
+
+        [Name("Enable Collision : Oil Barrel, Tent")]
+        [Description("If enabled, you will not be able to walk through these objects. If disabled, you can.")]
+        public bool diyBoardsBoolVal = true;
+        
+        /*
+        [Name("Num")]
+        [Description("Don't change it!! Default=19")]
+        [Slider(0, 31)]
+        public int diyBoardLayerNum = 19;
+        */
+
+    }
+
+
+    internal static class Settings
+    {
+        public static DiySettings options;
+
+        public static void OnLoad()
+        {
+            options = new DiySettings();
+            options.AddToModSettings("JTspectre's Item Pack", MenuType.Both);
+        }
+    }
+
+
+}
